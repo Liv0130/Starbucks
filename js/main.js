71e0e7cbfbbd6518f0e7ccba898b7a0aabe1b136
Liv0130 <yjjhsh27@gmail.com>
2022-02-09 14:42:15 +0900

    commom module

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-// when we click icon we can use search input and we call this function 'handler'
-
-searchInputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  // in this case we can set if sth sets as 'focused' do~
-  searchInputEl.setAttribute('placeholder', 'Search');
-  // attribute is for html element, placeholder for input element when it's empty, next thing for the letter would be there
-}); // until here we have made it happen so it should disappear again
-
-searchInputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  // remove 'focused'
-  searchInputEl.setAttribute('placeholder', '');
-  // and don't show anything on input part
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -149,6 +127,3 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
     // 보기쉽게 메소드들 체인형태로 배치, {}는 객체형
 });
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2022
